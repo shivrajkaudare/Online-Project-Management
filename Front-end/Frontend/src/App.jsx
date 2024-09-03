cc3c93487fd673a22cf9e1dccbeceee541bef21b
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-09-03 08:54:23 +0530

    update App.jsx and App.css

--- Front-end/Frontend/src/App.jsx
+++ Front-end/Frontend/src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Components/Login-Page/Login";
+import SideBar from "./Components/Home-Page/SideBar/SideBar";
+import Dashboard from "./Components/Home-Page/Dashbord/Dashbord";
+import NewProject from "./Components/Home-Page/NewProject/NewProject";
+import ProjectListing from "./Components/Home-Page/Project-listing/ProjectListing";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="MainContainer">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/*"
+            element={
+              <div className="container">
+                <div className="sidebar">
+                  <SideBar />
+                </div>
+                <div className="content">
+                  <Routes>
+                    <Route path="dashboard" element={<Dashboard />} />
+                    <Route path="allprojects" element={<ProjectListing />} />
+                    <Route path="addproject" element={<NewProject />} />
+                  </Routes>
+                </div>
+              </div>
+            }
+          />
+          <Route path="logout" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
