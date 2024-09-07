c4ec679be98947cceb56a2ec345109d94eb66061
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-09-07 19:15:24 +0530

    Add files via upload

---
+++ Front-end/Frontend/src/Components/Login-Page/Register.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Register.css"; // Add your custom styles
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/register",
+        formData
+      );
+      alert(response.data.message);
+      setFormData({
+        email: "",
+        password: "",
+      });
+    } catch (error) {
+      console.error("Error registering user:", error);
+      alert("Error registering user");
+    }
+  };
+
+  return (
+    <div className="register-container">
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
