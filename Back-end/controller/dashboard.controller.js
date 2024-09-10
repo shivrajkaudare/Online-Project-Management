178b83c5b61829689823d82da38d8d5f96073041
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-09-10 19:58:58 +0530

    Update dashboard.controller.js

---
+++ Back-end/controller/dashboard.controller.js
@@ -0,0 +1,29 @@
+// // Route to fetch dashboard stats
+const Project = require("../models/newProjectSchema.js");
+
+const dashboard = async (req, res) => {
+  try {
+    const totalProjects = await Project.countDocuments();
+    const closedProjects = await Project.countDocuments({ status: "Closed" });
+    const runningProjects = await Project.countDocuments({ status: "Running" });
+    const closureDelayProjects = await Project.countDocuments({
+      status: "Closure Delay",
+    });
+    const cancelledProjects = await Project.countDocuments({
+      status: "Cancelled",
+    });
+
+    res.json({
+      totalProjects,
+      closedProjects,
+      runningProjects,
+      closureDelayProjects,
+      cancelledProjects,
+    });
+  } catch (error) {
+    console.error("Error fetching dashboard stats:", error);
+    res.status(500).json({ message: "Error fetching dashboard stats" });
+  }
+};
+
+module.exports = { dashboard };
